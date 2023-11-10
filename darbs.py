983b25091f4e8fae10c24f90d8f340bc90cf5c11
edgarssniedze <edgars.sniedze@skolo.lv>
2023-11-10 16:12:56 +0200

    wwww

--- darbs.py
+++ darbs.py
@@ -10,14 +10,29 @@ cursor.execute('''
                     id INTEGER PRIMARY KEY,
                     vards TEXT NOT NULL,
                     uzvards TEXT NOT NULL,
-                    epasts TEXT NOT NULL UNIQUE
+                    epasts TEXT NOT NULL
                 )
                 ''')
 
+# izveido otru tabulu ar relaciju
 
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS pasutijumi(
+                    id INTEGER PRIMARY KEY,
+                    lietotaja_id INTEGER,
+                    produkta_nosaukums TEXT NOT NULL,
+                    daudzums INTEGER NOT NULL,
+                    FOREIGN KEY (lietotaja_id) REFERENCES lietotaji(id)
+                )   
+                ''')
 
+# ievieto datus lietotaju tabula
+cursor.execute("INSERT INTO lietotajs (vards, uzvards, epasts) VALUES (?,?,?)", ('Juris', 'Berzns', 'janis.berzins@p.lv'))
+cursor.execute("INSERT INTO lietotajs (vards, uzvards, epasts) VALUES (?,?,?)", ('janis', 'Berzns', 'janis2.berzins@p.lv'))
 
-
+# ievieto datus pasutitaju tabula
+cursor.execute("INSERT INTO pasutijumi (lietotaja_id, produkta_nosaukums, daudzums) VALUE (?,?,?)", (1, 'dators', 2))
+cursor.execute("INSERT INTO pasutijumi (lietotaja_id, produkta_nosaukums, daudzums) VALUE (?,?,?)", (2, 'televizors', 1))
 
 
 
